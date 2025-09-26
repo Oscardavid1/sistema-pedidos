80fe04a3e261423a8dc7d6a52ec7490eca1b463a
unknown <admin@BGAI201-008.bga.upb>
2025-09-25 20:18:48 -0500

    funcion Crear pedido

---
+++ ejercicio.js
@@ -0,0 +1,20 @@
+let productos = [
+  { idProducto: "1", nombre: "Hamburguesa", precio: 50000, categoria: 10 },
+  { idProducto: "2", nombre: "pan", precio: 80000, categoria: 5 },
+  { idProducto: "3", nombre: "pizza", precio: 120000, categoria: 3 }
+]
+
+let pedidos = []
+
+function pedido (idPedido, nombreCliente,estado,items) {
+    const orden = {
+        idPedido: 2,
+        nombreCliente,
+        estado: "pendiente",
+        items
+    }
+    pedidos.push(orden)
+    console.log(`pedido ${idPedido} creado para ${nombreCliente}, estado ${estado}`)
+}
+
+pedido (2,"Andres","pendiente",[{idProducto: "1", nombre: "Hamburguesa", precio: 50000, categoria: 10}])
